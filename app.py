aec7f6df83ff6b111db139f94e8f89e424c75025
DeZ <zhilindanilw@gmail.com>
2023-11-15 19:38:37 +0800

    основные запросы

--- app.py
+++ app.py
@@ -1,53 +1,125 @@
-from flask import Flask, request, jsonify
-from flask import SQLAclchemy
+import random
+from flask import *
+from flask_sqlalchemy import *
+from sqlalchemy import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hacatone.db'
-db = SQLAclchemy(app)
+db = SQLAlchemy(app)
+
+
+class Category(db.Model):
+    id = Column(Integer, primary_key=True, nullable=False)
+    category_name = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Category %r' % self.id
+
+
+class Document(db.Model):
+    id = Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    data = Column(db.String(500), nullable=False)
+    role_id = Column(Integer, ForeignKey('role.id', ondelete='CASCADE'), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id', ondelete='CASCADE'), nullable=False)
+
+    def __repr__(self):
+        return '<Document %r' % self.id
+
+
+class Employee(db.Model):
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    surname = Column(String(50), nullable=False)
+    patronymic = Column(String(50), nullable=False)
+    role_id = Column(Integer, ForeignKey('role.id', ondelete='CASCADE'), nullable=False)
+    date = Column(String(20))
+
+    def __repr__(self):
+        return '<Employee %r' % self.id
+
+
+class Login(db.Model):
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    employee_id = Column(Integer, ForeignKey('employee.id', ondelete='CASCADE'), nullable=False)
+    login = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Login %r' % self.id
+
+
+class Role(db.Model):
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    role = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Role %r' % self.id
+
+
+class Token(db.Model):
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    token = Column(String(300), nullable=False)
+    refreshtoken = Column(String(300), nullable=False)
+    employee_id = Column(Integer, ForeignKey('employee.id', ondelete='CASCADE'), nullable=False)
+
+    def __repr__(self):
+        return '<Token %r' % self.id
+
 
 @app.route('/api/mobile/registration', methods=['POST'])
 def registration():
     data = request.get_json()
 
-    name = data['name']
-    surname = data['surname']
-    patronymic = data['patronymic']
+    nm = data['name']
+    srnm = data['surname']
+    ptrnymc = data['patronymic']
     login = data['login']
     password = data['password']
 
-    #todo добавить получение токена из бд
-    token=''
-    refreshToken=''
+    try:
 
-    return jsonify({'token': token, 'refreshToken' : refreshToken})
+        employee = Employee.query.filter_by(name = nm, surname = srnm, patronymic=ptrnymc).first()
+
+        if Login.query.filter_by(employee_id = employee.id).first().login == login and Login.query.filter_by(employee_id=employee.id).first().password == password:
+            token = Token.query.filter_by(employee_id=employee.id).first()
+
+        return jsonify({'token': token.token, 'refreshToken': token.refreshtoken})
+    except:
+        return "Error"
 
 @app.route('/api/mobile/auth/', methods=['POST'])
 def auth():
     data = request.get_json()
 
-    login = data['login']
-    password = data['password']
+    lgn = data['login']
+    pas = data['password']
 
-    #todo Логика следующая: проверяет корректность данных по таблице 1.2, если всё верно возвращает  объект Если введен неправильный пароль кидает ошибку
+    try:
+        employeeLogin = Login.query.filter_by(login=lgn, password=pas).first()
+        token = Token.query.filter_by(employee_id=employeeLogin.employee_id).first()
 
-    result = True
+        return jsonify({'token': token.token, 'refreshToken': token.refreshtoken})
+    except:
+        return "Error, invalid login or password"
 
-    return result
 
-@app.route('/api/mobile/refresh', methods=['GET','POST'])
+@app.route('/api/mobile/refresh', methods=['POST'])
 def refresh():
     data = request.get_json()
 
-    refreshToken = data['refreshToken']
+    refreshToken = data['refreshtoken']
 
-    #todo Ищет этот токен в таблице 1.3, если находит выполняется следующая логика:
-    #     Запись в таблице с этим refreshToken модифицируется и token обновляется на новый сгенерируемый и возвращается объект с новыми данными
+    try:
+        token = Token.query.filter_by(refreshtoken=refreshToken).first()
 
-    # todo добавить получение токена из бд
-    token = ''
-    refreshToken = ''
+        token.refreshtoken=random.randint(20, 35) * 100
 
-    return jsonify({'token': token, 'refreshToken' : refreshToken})
+        db.session.commit()
+
+        return jsonify({'refreshToken': token.refreshtoken})
+    except:
+        return "Error, invalid refreshtoken"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
